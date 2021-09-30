c804bd590de52f7a14f1eb3a8f3da2aaab387ddd
Roch Blondiaux <contact@roch-blondiaux.com>
2021-09-30 21:51:02 +0200

    Spring Initializer

---
+++ src/test/java/com/roch/employees/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roch.employees.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
