98c69083f5fcfb376814c5f31252213fd1df5214
Roch Blondiaux <contact@roch-blondiaux.com>
2021-09-30 22:29:19 +0200

    Employee & Job proxies

--- src/main/java/com/roch/employees/app/repository/EmployeeProxy.java
+++ src/main/java/com/roch/employees/app/repository/EmployeeProxy.java
@@ -2,9 +2,11 @@ package com.roch.employees.app.repository;
 
 import com.roch.employees.app.CustomProperties;
 import com.roch.employees.app.model.Employee;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -20,6 +22,68 @@ public class EmployeeProxy {
 
     @Autowired
     private CustomProperties properties;
+
+    /**
+     * Create a new employee.
+     *
+     * @param employee to create.
+     * @return created employee or null if it failed.
+     */
+    public Employee create(@NonNull Employee employee) {
+        String baseURL = properties.getApiUrl() + "/employees";
+
+        RestTemplate template = new RestTemplate();
+        HttpEntity<Employee> httpEntity = new HttpEntity<>(employee);
+        ResponseEntity<Employee> response = template.exchange(
+                baseURL,
+                HttpMethod.POST,
+                httpEntity,
+                Employee.class);
+        log.debug("HTTP status for create employee: {}", response.getStatusCode());
+        return response.getBody();
+    }
+
+    /**
+     * Update an existing employee.
+     *
+     * @param employee to update.
+     * @return updated employee or null if it failed.
+     */
+    public Employee update(@NonNull Employee employee) {
+        String baseURL = properties.getApiUrl() + "/employees";
+
+        RestTemplate template = new RestTemplate();
+        HttpEntity<Employee> httpEntity = new HttpEntity<>(employee);
+        ResponseEntity<Employee> response = template.exchange(
+                baseURL,
+                HttpMethod.PATCH,
+                httpEntity,
+                Employee.class
+        );
+        log.debug("HTTP status for update employee: {}", response.getStatusCode());
+        return response.getBody();
+    }
+
+    /**
+     * Delete an existing employee.
+     *
+     * @param employee to delete
+     * @return true if it has been deleted successfully, otherwise false
+     */
+    public boolean delete(@NonNull Employee employee) {
+        String baseURL = properties.getApiUrl() + "/employees";
+
+        RestTemplate template = new RestTemplate();
+        HttpEntity<Employee> httpEntity = new HttpEntity<>(employee);
+        ResponseEntity<Void> response = template.exchange(
+                baseURL,
+                HttpMethod.DELETE,
+                httpEntity,
+                Void.class
+        );
+        log.debug("HTTP status for delete employee: {}", response.getStatusCode());
+        return response.getStatusCode().is2xxSuccessful();
+    }
 
     /**
      * Get all available employees from API.
--- src/main/java/com/roch/employees/app/repository/JobProxy.java
+++ src/main/java/com/roch/employees/app/repository/JobProxy.java
@@ -2,9 +2,12 @@ package com.roch.employees.app.repository;
 
 import com.roch.employees.app.CustomProperties;
 import com.roch.employees.app.model.Employee;
+import com.roch.employees.app.model.Job;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -20,6 +23,68 @@ public class JobProxy {
 
     @Autowired
     private CustomProperties properties;
+
+    /**
+     * Create a new job.
+     *
+     * @param job to create.
+     * @return created job.
+     */
+    public Job create(@NonNull Job job) {
+        String baseURl = properties.getApiUrl() + "/jobs";
+
+        RestTemplate template = new RestTemplate();
+        HttpEntity<Job> httpEntity = new HttpEntity<>(job);
+        ResponseEntity<Job> response = template.exchange(
+                baseURl,
+                HttpMethod.POST,
+                httpEntity,
+                Job.class);
+        log.debug("HTTP status for create job: {}", response.getStatusCode());
+        return response.getBody();
+    }
+
+    /**
+     * Update an existing job.
+     *
+     * @param job to update.
+     * @return updated job or null if it failed.
+     */
+    public Job update(@NonNull Job job) {
+        String baseURL = properties.getApiUrl() + "/job";
+
+        RestTemplate template = new RestTemplate();
+        HttpEntity<Job> httpEntity = new HttpEntity<>(job);
+        ResponseEntity<Job> response = template.exchange(
+                baseURL,
+                HttpMethod.PATCH,
+                httpEntity,
+                Job.class
+        );
+        log.debug("HTTP status for update job: {}", response.getStatusCode());
+        return response.getBody();
+    }
+
+    /**
+     * Delete an existing job.
+     *
+     * @param job to delete
+     * @return true if it has been deleted successfully, otherwise false
+     */
+    public boolean delete(@NonNull Job job) {
+        String baseURL = properties.getApiUrl() + "/employees";
+
+        RestTemplate template = new RestTemplate();
+        HttpEntity<Job> httpEntity = new HttpEntity<>(job);
+        ResponseEntity<Void> response = template.exchange(
+                baseURL,
+                HttpMethod.DELETE,
+                httpEntity,
+                Void.class
+        );
+        log.debug("HTTP status for delete job: {}", response.getStatusCode());
+        return response.getStatusCode().is2xxSuccessful();
+    }
 
     /**
      * Get all available jobs from API.
